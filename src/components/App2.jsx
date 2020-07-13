bb86850f407805f69bb1bee169d08d11348651b2
htcongvn <40765787+htcongvn@users.noreply.github.com>
2020-07-13 00:21:23 -0700

    Starting a custom react app source code

--- src/components/App2.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MySimplePage from "./MySimplePage";
-
-function App2() {
-  return <MySimplePage />;
-}
-
-export default App2;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-/* eslint-disable react/jsx-no-undef */
-/* eslint-disable react/prop-types */
-
-// Latest feature to come to Javascript (ES6+) is the import ad export keywords
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-import App1 from "./components/App1";
-import App2 from "./components/App2";
-import App3 from "./components/App3";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<App1 />, document.getElementById("root_1"));
-ReactDOM.render(<App2 />, document.getElementById("root_2"));
-ReactDOM.render(<App3 />, document.getElementById("root_3"));
