294d6c2114c8f9a63a721b37279dd3867184866c
htcongvn <40765787+htcongvn@users.noreply.github.com>
2020-07-17 00:07:55 -0700

    HelloLoginApp-Complete: React Conditional Rendering with the Ternary Operator & AND Operator

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Input(props) {
+  return <input type={props.type} placeholder={props.placeholder} />;
+}
+
+export default Input;
