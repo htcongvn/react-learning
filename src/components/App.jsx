93be78d1a9a22502db9e3556079c94d9c284e37e
htcongvn <40765787+htcongvn@users.noreply.github.com>
2020-07-20 14:58:15 -0700

    Event Handling and Form Submision in React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [headingText, setHeadingText] = useState("Hi");
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [name, setName] = useState("");
+
+  function handleClick(event) {
+    const [greetingVerb] = headingText.split(" ");
+    setHeadingText((greetingVerb === "Hello" ? "Bonjour " : "Hello ") + name);
+
+    event.preventDefault();
+  }
+
+  function handleMouseOver() {
+    setIsMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setIsMouseOver(false);
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleChange}
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          type="submit"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
