b3dadc9f6129fd1f7e367795408fc1683caaf2ce
htcongvn <40765787+htcongvn@users.noreply.github.com>
2020-07-19 17:30:47 -0700

    React hooks: useState, useEffect, useRef and Custom Hook - useInterval

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const now = new Date().toLocaleTimeString()
+  const [time, setTime] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [isRunning, setIsRunning] = useState(true);
 
-  function increaseCount() {
-    setCount(count + 1);
+  useEffect(() => {
+    document.title = `Delay is just updated to ${delay} ms`;
+  }, [delay]); // Only re-run this effect if delay changes , aka. browser tab title is just updated once delay changes
+
+  useInterval(
+    () => {
+      // Your custom logic here
+      setTime(time + 1);
+    },
+    isRunning ? delay : null
+  ); // Only re-run this custom hook if delay changes, aka a new interval will be set once delay changes
+
+  function handleDelayChange(e) {
+    setDelay(Number(e.target.value));
   }
-
-  function decreaseCount() {
-    setCount(count - 1);
+  function handleToggleTimeChange() {
+    setIsRunning(!isRunning);
   }
 
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increaseCount}>+</button>
-      <button onClick={decreaseCount}>-</button>
+      <h1>{time}</h1>
+      <input value={delay} onChange={handleDelayChange} />
+      <button onClick={handleToggleTimeChange}>
+        {isRunning ? "Turn Off" : "Turn On"} Timer
+      </button>
+      <button onClick={handleDelayChange} value="1000">
+        Set Timer 1s
+      </button>
     </div>
   );
 }
